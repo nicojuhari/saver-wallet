397677784b5d0f7b69b5a331b93bca1a3d02b540
Nicolae Cojuhari <nicojuhari@gmail.com>
2024-10-18 20:52:27 +0200

    feat: some animation on home page

--- components/EmptyWallet.vue
+++ components/EmptyWallet.vue
@@ -2,41 +2,41 @@
     
     const { isLoggedIn } = useAuth()
     
-    const cardRotations = [.85, .9, .95, 1]
-    const cardColors = ['bg-green-500', 'bg-yellow-500', 'bg-red-500', 'bg-blue-500']
+    // Reverse the order of the arrays
+    const cardRotations = [1, .95, .9, .85];
+    const cardColors = ['bg-blue-500', 'bg-red-500', 'bg-yellow-500', 'bg-green-500'];
 
 </script>
 
 <template>
-    <div class="my-10 relative h-40 flex justify-center">
+    <div class="my-10 sticky h-52 flex justify-center">
         <div
             v-for="(item, idx) in cardRotations"
             :key="idx"
             class="credit-card absolute bottom-0"
-            :class="cardColors[idx]"
+            :class="[cardColors[idx], {'animate-card opacity-0':  idx != 0}]"
             :style="{
-                transform: `scale(${item}) translateY(${idx * 12}px)`,
+                transform: `scaleX(${item}) translateY(${idx * -10}px)`,
+                animationDelay: `${(idx) * 0.15}s`,
+                zIndex: -1 * idx ,
+                top: `${idx * 10}px`// Adjust the delay as needed
             }"
         >
-            <template v-if="idx==3">
-                <nuxt-link v-if="!isLoggedIn" to="/cards/new" class="flex items-center justify-center relative h-full">
-                    <div class="text-white flex gap-4 items-center">
-                        <Icon name="i-ph-plus-light" class="text-4xl" />
-                        <span class="text-xl">Add a Card</span>
-                    </div>
-                </nuxt-link>
-                <nuxt-link v-else to="/cards" class="flex items-center justify-center relative h-full">
-                    <div class="text-white flex gap-4 items-center">
-                        <Icon name="i-ph-eye-light" class="text-4xl" />
-                        <span class="text-xl">View your Cards</span>
-                    </div>
-                </nuxt-link>
-            </template>
-            
+           
+        <template v-if="idx == 0">
+            <nuxt-link v-if="!isLoggedIn" to="/cards/new" class="flex items-center opacity-0 fade-text justify-center relative h-full shrink-0">
+                <div class="text-white flex gap-4 items-center">
+                    <Icon name="i-ph-plus-light" class="text-4xl" />
+                    <span class="text-xl">Add a Card</span>
+                </div>
+            </nuxt-link>
+            <nuxt-link v-else to="/cards"  class="flex items-center opacity-0 fade-text justify-center relative h-full shrink-0">
+                <div class="text-white flex gap-4 items-center">
+                    <Icon name="i-ph-eye-light" class="text-4xl" />
+                    <span class="text-xl">View your Cards</span>
+                </div>
+            </nuxt-link>
+        </template>
         </div>
     </div>
 </template>
-
-<style scoped>
-
-</style>
