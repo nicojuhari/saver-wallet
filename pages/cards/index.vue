b637d9920f7c0e7577e639e3ae447fa93c5d5ced
Nicolae Cojuhari <nicojuhari@gmail.com>
2024-10-18 16:29:25 +0200

    fix: zoom for shared cards

--- pages/cards/index.vue
+++ pages/cards/index.vue
@@ -8,6 +8,7 @@
     const sharedCards = ref()
     const cardsLayout = ref('slider')
     const zoomCard = ref()
+    const isShared = ref(false)
 
     if(user.value) {
         let res = await getCardsByUserId(user.value?.$id)
@@ -25,12 +26,14 @@
         else return 'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'
     })
 
-    const openZoom = (card:any) => {
+    const openZoom = (card:any, shared: boolean = false) => {
         zoomCard.value = card
+        isShared.value = shared
     }
 
     const closeZoom = () => {
         zoomCard.value = null
+        isShared.value = false
     }
 
 
@@ -60,13 +63,14 @@
         <div v-else class="text-2xl text-center my-10">
             You don't have any cards yet
         </div>
-
+        
+        <!-- SHARED CARDS -->
         <div class="mt-16">
-            <div class="text-lg font-medium"> Shared with yo ({{ sharedCards?.length }})</div>
-            <div class="text-sm text-gray-600">click on card to zoom it</div>
+            <div class="text-lg font-medium"> Shared with you ({{ sharedCards?.length }})</div>
+            <div class="text-sm text-gray-600">click on card to zoom in/out</div>
         </div>
         <div v-if="sharedCards?.length" class="mx-auto py-2.5" :class="cardsLayoutStyles">
-            <div v-for="card in sharedCards" :key="card.$id" @click="openZoom(card.viewUrl)" 
+            <div v-for="card in sharedCards" :key="card.$id" @click="openZoom(card, true)" 
                class="space-y-2.5 snap-center shrink-0" :class="{'mx-auto' : cardsLayout === 'grid'}">
                 <img :src="card.viewUrl" class="credit-card shadow-md"/>
                 <div class="text-center truncate">{{ card.title }}</div>
@@ -81,13 +85,12 @@
             <div v-if="zoomCard" class="fixed inset-0 z-10 bg-black bg-opacity-90">
                 <div class="p-4 bg-white bg-opacity-5 text-xl truncate text-white absolute top-0 left-0 w-full">{{ zoomCard.title }}</div>
                 <div class="zoom-block shrink-0 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 absolute w-full h-auto max-w-[460px] p-4">
-                    <img :src="zoomCard.viewUrl"  class="zoom-card-active w-full object-cover rounded-xl aspesct-[646/408]" />
+                    <img :src="zoomCard.viewUrl"  class="zoom-card-active w-full object-cover rounded-xl aspect-[646/408]" />
                 </div>
                 <div class="zoom-card-tools flex gap-8 justify-between py-2 px-1 items-center text-white absolute bottom-10 left-1/2 -translate-x-1/2">
-                    <nuxt-link :to="`/cards/${zoomCard.card_id}`" class="btn-square bg-white shrink-0 !bg-opacity-30">
+                    <nuxt-link v-if="!isShared" :to="`/cards/${zoomCard.card_id}`" class="btn-square bg-white shrink-0 !bg-opacity-30">
                         <Icon name="i-ph-pencil-line-light"/>
                     </nuxt-link>
-                    
                     <button @click="closeZoom" class="btn-square bg-white shrink-0 !bg-opacity-30">
                         <Icon name="i-ph-x"/>
                     </button>
@@ -111,11 +114,13 @@
 
 @media (max-width: 1000px) and (orientation: landscape) {
   .zoom-card-tools {
-    right: 0;
+    right: 0px;
     flex-direction: column;
     width: 40px;
     left: auto;
-    bottom: 20px;
+    top: 50%;
+    bottom: auto;
+    --tw-translate-y: -50%;
   }
 }
 </style>
