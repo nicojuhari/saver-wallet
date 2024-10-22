6796283face898030e7c1fd5545e200310cdcbcb
Nicolae Cojuhari <nicojuhari@gmail.com>
2024-10-22 13:59:04 +0200

    redesign cards page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,17 @@
+<script setup lang="ts">
+  const { isLoggedIn } = useAuth()
+</script> 
 <template>
   <div class="min-h-screen flex flex-col bg-gray-50">
     <IncludesHeader/>
     <main class="min-h-dvh py-10">
       <slot></slot>
     </main>
+    <div v-if="isLoggedIn" class="container mt-auto relative">
+      <nuxt-link to="/cards/new" title="Add a new card" class="btn btn-round bg-red-600 fixed md:absolute right-2 z-10 bottom-2">
+              <Icon name="i-ph-plus"/>
+          </nuxt-link>
+    </div>
     <IncludesFooter class="mt-auto" />
   </div>
 </template>
--- pages/cards/index.vue
+++ pages/cards/index.vue
@@ -18,40 +18,43 @@
 </script>
 <template>
     <div class="container">
-        <div class="flex items-center gap-4 justify-between px-1.5 mb-2">
-            <div>
-                <div class="text-lg font-medium"> Your cards ({{ allCards?.length }})</div>
-                <div class="text-sm text-gray-600">click on card to zoom in/out</div>
-            </div>
+        <div class="flex items-center gap-4 justify-between">
+            <div class="text-lg font-medium">Your cards ({{ allCards?.length }})</div>
             <div class="flex gap-4 items-center">
                 <label class="flex flex-row gap-4 items-center cursor-pointer">
                     <CheckboxRoot
                         v-model:checked="editorMode"
-                        class="flex h-6 w-6 appearance-none items-center justify-center rounded-md  bg-white shadow-md outline-none focus-within:shadow-xl"
+                        class="flex h-6 w-6 appearance-none items-center justify-center rounded-md  bg-white border outline-none border-gray-600"
                     >
-                        <CheckboxIndicator class="bg-white h-full w-full rounded flex items-center justify-center">
+                        <CheckboxIndicator class="bg-gray-600 h-full w-full rounded flex items-center justify-center">
                         <Icon
                             name="i-ph-check"
-                            class="text-blue-600 text-lg"
+                            class="text-white text-lg"
                         />
                         </CheckboxIndicator>
                     </CheckboxRoot>
-                    <span class="select-none">Editor mode?</span>
+                    <span class="select-none">Editor mode</span>
                 </label>
-                 <nuxt-link to="/cards/new" title="Add a new card" class="shrink-0 h-10">
-                    <Icon name="i-ph-plus-circle-thin" class="text-[40px] text-red-600"/>
-                </nuxt-link>
             </div>
         </div>
         <template v-if="allCards?.length">
-            <CardCarousel v-if="!editorMode" :cards="allCards"/>
+            <template v-if="!editorMode">
+                <CardCarousel  :cards="allCards"/>
+                <div class="flex items-center gap-3 mt-6">
+                    <Icon name="i-ph-lightbulb-filament-duotone" class="text-green-600 text-2xl md:text-xl shrink-0"/>
+                    <ul class="space-y-2 text-gray-500 text-sm">
+                        <li>Click on a card to zoom in/out.</li>
+                        <li><strong>Enable Editor Mode</strong> to update, share, or delete your card.</li>
+                    </ul>
+                </div>
+            </template>
             <CardGrid v-else :cards="allCards" />
         </template>
-        <div v-else class="text-2xl text-center my-10">
-            You don't have any cards yet <nuxt-link to="/cards/new" title="Add a new card" class="shrink-0 h-10">
-                    <Icon name="i-ph-plus-circle-thin" class="text-[40px] text-red-600"/>
-                </nuxt-link>
-        </div>
+        <nuxt-link to="/cards/new" v-else class="credit-card flex item justify-center items-center gap-4 bg-gray-200">
+
+           <Icon name="i-ph-plus-light" class="text-4xl" />
+                    <span class="text-xl">Add a Card</span>
+        </nuxt-link>
     </div>
 </template>
 <!-- <style>
